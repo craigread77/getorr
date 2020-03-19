da9f4177c115c6af8f14b4b559a26e75f24cdf98
craigread77 <craigread77@gmail.com>
2020-03-19 00:00:03 -0500

    v1 working. Needs improvements to how the file size is pulled

--- get_torrent.pl
+++ get_torrent.pl
@@ -6,13 +6,35 @@ use warnings;
 use lib 'lib';
 use Getorr;
 
+my $DEBUG;
+
 # Get name of movie
-my $name = $ARGV[0];
+my $name = $ARGV[0] or die "Usage: get_torrent.pl <Name of Movie>";
+my $words_match = join('|', split(' ', $name));
+my $magnet;
 
 
-# TODO pull html to get magnet link
-Getorr->fetch_leet_torrent($name);
+# Returns list of torrent hashesfrom 1337x.to
+# keys are (url, title, size, seeders)
+my @torrents = Getorr->fetch_leet_torrent($name);
+
+for my $i (0 .. $#torrents) {
+    next if !$torrents[$i]{size}  || $torrents[$i]{size} > 2048; # Don't download files over 2GB
+    next if $torrents[$i]{title} !~ /$words_match/i;
+    
+    say "---------------------------------";
+    say "Title: ". $torrents[$i]{title};
+    say "Seeders: ". $torrents[$i]{seeds};
+    say "Size: ". $torrents[$i]{size} . " MB";
+    say '---------------------------------';
+    
+    
+    $magnet = Getorr->get_magnet_link_leet($torrents[$i]{url});
+    last if $magnet; # End loop if magnet link was returned
+}
 
 
+# Download using qbittorrent
+Getorr->download_torrent_leet($magnet) unless $DEBUG;
 
-# TODO download torrent using qbt
+
--- lib/Getorr.pm
+++ lib/Getorr.pm
@@ -5,6 +5,21 @@ use warnings;
 use WWW::Mechanize;
 use WWW::Mechanize::TreeBuilder;
 
+sub convert_mb_leet {
+    my $size = $_[0];
+    $size // return undef;
+    
+    if ($size =~ /MB/) {
+        $size =~ s/ MB//;
+        return $size;
+    }
+    else {
+        $size =~ s/ GB//;
+        return int($size * 1024);
+    }
+    
+}
+
 sub fetch_tpb_torrent {
     my $self = shift;
     my $name = shift;  
@@ -23,37 +38,62 @@ sub fetch_leet_torrent {
     my $url_name = $name =~ s/\s+/%20/gr; # Replace spaces with '%20'
     my $url = "https://www.1377x.to/search/$url_name/1/";
     my $mech = WWW::Mechanize->new();
-    my %torrents;
+    my @torrents;
+    my $torrent_url;
+    my $title;
+    my $count = 0;
+    
     
     WWW::Mechanize::TreeBuilder->meta->apply($mech);
     
     $mech->get( $url );
+    
     # Get info from torrent table
+    my @rows      =  $mech->look_down( class   => 'coll-1 name' );
+    my @seeders   =  $mech->look_down( class   => 'coll-2 seeds' );
+    my @sizes_alt =  $mech->look_down( class   => 'coll-4 size mob-uploader'); # Some titles use a different class for file size
+    my @sizes     =  $mech->look_down( class   => 'coll-4 size mob-vip' );
     
-    my @rows = $mech->look_down(
-        #class => 'coll-1 name'
-        _tag => 'tr'
-    );
-    
-    for (@rows) {
+    # Skip first line (headers)
+    for (splice(@rows, 1)) {
         my $data = $_->as_XML();
-        $data =~ /(\/torrent\/\d+\/\S+\/)"/;
-        my $url = 'https://www.1377x.to' . $1;
+        if ($data =~ m{(/torrent/\d+/.+/)">(.+)</a>}s) {
+            $torrent_url = $1;
+            $title = $2;
+        } else {
+            warn "No match!: $!";
+        }
         
-        # Need to update this to go based on individual tags
-        $data =~ /\/torrent\/\d+\/(\S+)\/".+<td class="coll-2 seeds">(\d+)<\/td><td class="coll-3 leeches">(\d+)<\/td>.+<td class="coll-4 size mob-uploader">(.+)<span/;
-        my ($title, $seeders, $leechers, $size) = ($1, $2, $3, $4);
+        $torrents[$count]{url} = 'https://1337x.to' . $torrent_url;
+        $torrents[$count]{title} = $title;
         
-        say "URL: " . $url;
-        say "Title: " . $title;
-        say "Seeders: " . $seeders;
-        say "Leechers: " . $leechers;
-        say "Size: " . $size;
-    
+        $count++
     }
     
+    $count = 0;
     
+    for (@seeders) {
+        my $seeds = $_->as_text();
+        $torrents[$count]{seeds} = $seeds;
+        $count++;
+    }
     
+    $count = 0;
+    
+    push(@sizes, @sizes_alt); 
+    for (@sizes) {
+        my $size = $_->as_XML();
+        if ($size =~ m{(\d+\.\d+ GB)|(\d+\.\d+ MB)|(\d,\d+\.\d+ MB)}s) {
+            $size = convert_mb_leet($1);
+            $size =~ s/"//g if $size;
+        }
+        
+        $torrents[$count]{size} = $size;
+        $count++;
+      
+    }
+    
+    return @torrents;
     
 }
 
@@ -62,6 +102,40 @@ sub fetch_rarbg_torrent {
     my $name = shift;
     my $url_name = $name =~ s/\s+/\+/gr; # Replace spaces with '+'
     my $url = "http://rarbg.to/torrents.php?search=$name";
+}
+
+sub get_magnet_link_leet {
+    my $self = shift;
+    my $url = shift;
+    my $mech = WWW::Mechanize->new();
+    WWW::Mechanize::TreeBuilder->meta->apply($mech);
+    my $magnet;
+    my @links;
+    
+    $mech->get( $url );
+    
+    @links = $mech->look_down(
+        class => 'l8e38fe43402620d7ba3be520d79ebba7c45382ce l7cd3af1a2fc1ad3785dcbd482bc889ca0e47f69d l3e5e5898ac3e4f296d17506223739c9666af7cb1');
+    
+    # Try alternative magnet class
+    if (! $links[0]) {
+        @links = $mech->look_down(
+            class => 'lf5cab25abeea2db047987e779843b6bb1c0aa307 l642d2dc206fb87702b09284b24d4f5a1919a84d3 lf134dbe95e3f85ce836b190e341297de8a86d431');
+    }
+    
+    if ($links[0]->as_XML() =~ m{href="(.+)" onclick}s) {
+        $magnet = $1;
+    } else { return undef };
+    
+    return $magnet;
+}
+
+sub download_torrent_leet {
+    my $self = shift;
+    my $magnet = shift;
+    
+    my $script = 'qbt torrent add url "' . $magnet . '"';
+    `$script`;
 }
     
 
